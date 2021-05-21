c98c06f3fef6d047b018ba0085a9e2cf8c6fa6f6
milangeorge2000 <45516866+milangeorge2000@users.noreply.github.com>
2021-05-21 11:16:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-import numpy as np
 import os
 from facenet_pytorch import MTCNN, InceptionResnetV1
 import torch
 from torch.utils.data import DataLoader
 from PIL import Image
-from flask_ngrok import run_with_ngrok
-import webbrowser
-
+mtcnn = torch.load("data1.pt")
+resnet = torch.load("data3.pt")
+saved_data = torch.load('data2.pt') # loading data.pt file
+        
 
 # Define a flask app
 app = Flask(__name__)
-
-# run_with_ngrok(app)
 
 
 @app.route('/', methods=['GET'])
@@ -26,24 +24,16 @@ def index():
 def upload():
     if request.method == 'POST':
         f = request.files['file']
-        f.save("img.jpg")
-    
-        
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
+        img.save("img.jpg")
 
-        mtcnn = MTCNN(image_size=240, margin=0, min_face_size=20) # initializing mtcnn for face detection
-        resnet = InceptionResnetV1(pretrained='vggface2').eval() # initializing resnet for face img to embeding conversion
-        
+ 
         def face_match(img_path, data_path): # img_path= location of photo, data_path= location of data.pt 
             # getting embedding matrix of the given img
             img = Image.open(img_path)
             face, prob = mtcnn(img, return_prob=True) # returns cropped face and probability
             emb = resnet(face.unsqueeze(0)).detach() # detech is to make required gradient false
             
-            saved_data = torch.load('data2.pt') # loading data.pt file
+            
             embedding_list = saved_data[0] # getting embedding data
             name_list = saved_data[1] # getting list of names
             dist_list = [] # list of matched distances, minimum distance is used to identify the person
